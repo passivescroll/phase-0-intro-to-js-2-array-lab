ad17ad6f56f99ae1e0b933b8adc0872e7c193b8c
sky <flahertysky12@gmail.com>
2021-05-06 13:51:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']; 
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    
+}
+function destructivelyRemoveFirstCat() {
+     cats.shift();
+} 
+
+function appendCat(name) {
+    const catsArray = [...cats]
+    catsArray.push(name)
+    return catsArray
+}
+
+
+function prependCat(name) {
+    const catsArray = [...cats]
+    catsArray.unshift(name)
+    return catsArray
+}
+
+function removeLastCat() {
+    const catsArray = [...cats]
+    catsArray.pop()
+    return catsArray
+}
+
+function removeFirstCat() {
+    const catsArray = [...cats]
+    catsArray.shift()
+    return catsArray
+}
+        
+
+ 
